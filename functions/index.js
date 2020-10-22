2fdf583ac870a7bf9e5d2033f083d4e7f2a10320
eliyahu porush <eliyahuporush@gmail.com>
2020-10-22 18:14:39 +0300

    make it better

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,16 @@
 const functions = require('firebase-functions');
 const express = require('express') ;
-const app = express();
+const app = require('../app.js'); ;
+const app1 = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app1.get('/hello', (req, res)  => {
+    res.send(JSON.stringify('hello world'))
+})
+
+
+exports.helloWorld = functions.https.onRequest(app1) ;
 
 exports.app = functions.https.onRequest(app) ;
